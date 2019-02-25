eb7baacba3e7c9e42179266d453bf37a18dc6e96
ChopeyDmitry <46756756+ChopeyDmitry@users.noreply.github.com>
2019-02-25 18:05:21 +0200

    hw

--- second-Selenium-Project/src/test/java/com/tran/ebay/app/ApplicationManager.java
+++ second-Selenium-Project/src/test/java/com/tran/ebay/app/ApplicationManager.java
@@ -3,6 +3,8 @@ package com.tran.ebay.app;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,8 +12,19 @@ public class ApplicationManager {
     private ItemHelper item;
     private UserHelper user;
     WebDriver wd;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        }
+
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         wd.manage().window().maximize();
@@ -19,7 +32,6 @@ public class ApplicationManager {
         item = new ItemHelper(wd);
         user = new UserHelper(wd);
     }
-
 
 
     public void openSite(String url) {
--- second-Selenium-Project/src/test/java/com/tran/ebay/tests/TestBase.java
+++ second-Selenium-Project/src/test/java/com/tran/ebay/tests/TestBase.java
@@ -1,12 +1,13 @@
 package com.tran.ebay.tests;
 
 import com.tran.ebay.app.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
-    protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeClass
     public void setUp() {
